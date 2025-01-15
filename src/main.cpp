ab5b8048e106331cdca4312c290b8ee9cbda8014
cra2y8eans <crazybeans@163.com>
2025-01-15 17:45:28 +0800

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include "batteryReading.hpp"
+#include "intervalByMillis.hpp"
 
 void setup() {
 }
